fd8e7637ee04cf417e276cff01c84b889d31a826
orangedenshi <orangedenshikk@gmail.com>
2024-11-12 18:56:58 +0900

    Initial Commit

---
+++ testBle/testBleApp.swift
@@ -0,0 +1,17 @@
+//
+//  testBleApp.swift
+//  testBle
+//
+//  Created by nobuaki on 2024/11/12.
+//
+
+import SwiftUI
+
+@main
+struct testBleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testBleTests/testBleTests.swift
@@ -0,0 +1,17 @@
+//
+//  testBleTests.swift
+//  testBleTests
+//
+//  Created by nobuaki on 2024/11/12.
+//
+
+import Testing
+@testable import testBle
+
+struct testBleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
